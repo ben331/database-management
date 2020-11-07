15488c5fef33b56fa8d35216f0d1eeb882b75a67
Benjamin <54720004+ben331@users.noreply.github.com>
2020-11-06 22:02:26 -0500

    Creation of fxml files

--- src/ui/ControllerGUI.java
+++ src/ui/ControllerGUI.java
@@ -1,6 +1,11 @@
 package ui;
 
 import model.DataBase;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Rectangle;
 
 public class ControllerGUI {
 
@@ -9,5 +14,44 @@ public class ControllerGUI {
 	public ControllerGUI() {
 		dataBase = new DataBase();
 	}
+	
+	//Main scene---------------------------------------------------------------------------------------
+	//Atributtes-------------------------------------------------------------------
+	@FXML
+    private BorderPane mainPane;
+    
+    //Methods----------------------------------------------------------------------
+    
+    @FXML
+    void about(ActionEvent event) {
+
+    }
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openPaneToAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openPaneToSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveData(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showPaneToGenerate(ActionEvent event) {
+
+    }
+    
+    //
 	
 }
